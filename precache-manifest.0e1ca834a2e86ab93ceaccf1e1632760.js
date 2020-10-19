eace0e133651b2ab7dd02e1a025929115c0896c1
Priyanshu Dwivedi <ankurdwivedi75@gmail.com>
2020-10-19 22:26:52 +0530

    Updates

---
+++ precache-manifest.0e1ca834a2e86ab93ceaccf1e1632760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfdde6ab1e18a375fc1c7e0477b478d",
+    "url": "/react-django-jwt-auth-frontend/index.html"
+  },
+  {
+    "revision": "11e4933bc00c09635427",
+    "url": "/react-django-jwt-auth-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc7e86cb1cb69723d289",
+    "url": "/react-django-jwt-auth-frontend/static/js/2.0804bf51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-django-jwt-auth-frontend/static/js/2.0804bf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e4933bc00c09635427",
+    "url": "/react-django-jwt-auth-frontend/static/js/main.adeb74dd.chunk.js"
+  },
+  {
+    "revision": "ec076a1a2ac4cbf93c76",
+    "url": "/react-django-jwt-auth-frontend/static/js/runtime-main.bf16c9c4.js"
+  }
+]);
